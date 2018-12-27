d1bb4b45012d0ab00cc5b233ae79b63ad5243f94
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-12-28 05:13:00 +0700

    Import only minify file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import 'jquery'
-import 'popper.js'
-import 'bootstrap'
-import 'bootstrap/scss/bootstrap.scss'
+import 'jquery/dist/jquery.min.js'
+import 'popper.js/dist/popper.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
